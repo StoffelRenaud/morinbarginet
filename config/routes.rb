bdf6e4cc33001c2b046ac59ca6e6ce8132621920
renaudstoffel <stoffelrenaud@gmail.com>
2018-09-27 14:28:30 +0200

    Routes Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :owner do
+    resources :reservations, only: [:new, :create, :edit, :update, :destroy]
+    resources :topics, only: [:new, :create, :edit, :update, :destroy]
+    resources :posts, only: [:new, :create, :edit, :update, :destroy] do
+      resources :photos, only: [:create, :destroy]
+    end
+  end
+
+  resources :reservations, only: [:index, :show]
+  resources :topics, only: [:index, :show]
+  resources :posts, only: [:index, :show] do
+    resources :photos, only: [:index, :show, :update]
+  end
 end
