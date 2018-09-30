f48e72a388470fc40972821bd37d82c3371dcbcc
renaudstoffel <stoffelrenaud@gmail.com>
2018-09-30 16:23:36 +0200

    devise css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_132317) do
+ActiveRecord::Schema.define(version: 2018_09_30_140441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2018_09_30_132317) do
     t.string "parent"
     t.string "name"
     t.string "admin", default: "f"
-    t.string "telephone"
     t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
